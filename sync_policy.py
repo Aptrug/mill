9c3ac6bbd3abc3686fcfd94787993f83c99873fd
anon <anon@example.com>
2026-04-26 19:25:46 +0100

    sync_policy.py

--- sync_policy.py
+++ sync_policy.py
@@ -6,15 +6,15 @@
 import sys, json
 
 settings_path = sys.argv[1]
-MILL_URL	  = sys.argv[2]
-desired_ids   = sys.argv[3:]
+MILL_URL = sys.argv[2]
+desired_ids = sys.argv[3:]
 
 with open(settings_path) as f:
 	data = json.load(f)
 
-entries   = data.get("ExtensionInstallForcelist", [])
+entries = data.get("ExtensionInstallForcelist", [])
 non_owned = [e for e in entries if e.split(";", 1)[1] != MILL_URL]
-owned	  = [f"{i};{MILL_URL}" for i in desired_ids]
+owned = [f"{i};{MILL_URL}" for i in desired_ids]
 
 data["ExtensionInstallForcelist"] = non_owned + owned
 
