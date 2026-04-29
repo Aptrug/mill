18930897d168d6cb2964c4094776dd23e2162f41
anon <anon@example.com>
2026-04-29 16:38:17 +0100

    content.js

--- FeedWatcher/src/content.js
+++ FeedWatcher/src/content.js
@@ -150,27 +150,6 @@ function persistSeen() {
 }
 
 /* ------------------------------------------------------------------ */
-/* Banner observer (document-scoped, started once at init)             */
-/* ------------------------------------------------------------------ */
-
-function onBannerMutation(mutations) {
-	const n = mutations.length;
-	for (let i = 0; i < n; i++) {
-		const added = mutations[i].addedNodes;
-		const m = added.length;
-		for (let j = 0; j < m; j++) {
-			const node = added[j];
-			if (node.nodeType !== 1)
-				continue;
-			if (node.matches(BANNER_SEL) || node.querySelector(BANNER_SEL) !== null) {
-				clickRefreshBanner();
-				return;
-			}
-		}
-	}
-}
-
-/* ------------------------------------------------------------------ */
 /* Mutation handler (hot path)                                          */
 /* ------------------------------------------------------------------ */
 
